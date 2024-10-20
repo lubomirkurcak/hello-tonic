841696fbca115990d13534cdb1f8e778b75ef76f
Ľubomír Kurčák <lubomirkurcak@gmail.com>
2024-10-21 00:33:58 +0200

    guide server grpc example

--- build.rs
+++ build.rs
@@ -2,5 +2,6 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     tonic_build::compile_protos("proto/hello.proto")?;
+    tonic_build::compile_protos("proto/route.proto")?;
     Ok(())
 }
